52af8efe2dae14d174eeab26477374c9c3161550
Dave G <dpg3k@virginia.edu>
2026-03-09 09:59:34 -0400

    Add the reconciler

---
+++ aptrust-submit-reconciler/config.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"log"
+)
+
+// ServiceConfig defines all the service configuration parameters
+type ServiceConfig struct {
+	// queue definitions
+	InQueueName   string // SQS queue name to monitor for message
+	PollTimeOut   int32  // the SQS queue timeout (in seconds)
+	HeartbeatTime int32  // the SQS queue heartbeat time (in seconds)
+
+	// event bus definitions
+	BusName        string // the event bus name
+	BusEventSource string // the source of published events
+
+	// database configuration
+	DbHost     string // database host
+	DbPort     int    // database port
+	DbName     string // database name
+	DbUser     string // database user
+	DbPassword string // database password
+}
+
+// LoadConfiguration will load the service configuration from env/cmdline
+// and return a pointer to it. Any failures are fatal.
+func LoadConfiguration() *ServiceConfig {
+
+	var cfg ServiceConfig
+
+	// queue definitions
+	cfg.InQueueName = ensureSetAndNonEmpty("NOTIFY_IN_QUEUE")
+	cfg.PollTimeOut = int32(envToInt("NOTIFY_QUEUE_POLL_TIMEOUT"))
+	cfg.HeartbeatTime = int32(envToInt("NOTIFY_QUEUE_HEARTBEAT_TIME"))
+
+	// event bus definitions
+	cfg.BusName = envWithDefault("EVENT_BUS_NAME", "")
+	cfg.BusEventSource = envWithDefault("EVENT_SRC_NAME", "")
+
+	// database definitions
+	cfg.DbHost = ensureSetAndNonEmpty("DB_HOST")
+	cfg.DbPort = envToInt("DB_PORT")
+	cfg.DbName = ensureSetAndNonEmpty("DB_NAME")
+	cfg.DbUser = ensureSetAndNonEmpty("DB_USER")
+	cfg.DbPassword = ensureSetAndNonEmpty("DB_PASSWORD")
+
+	// queue definitions
+	log.Printf("[CONFIG] InQueueName     = [%s]", cfg.InQueueName)
+	log.Printf("[CONFIG] PollTimeOut     = [%d]", cfg.PollTimeOut)
+	log.Printf("[CONFIG] HeartbeatTime   = [%d]", cfg.HeartbeatTime)
+
+	// event bus definitions
+	log.Printf("[CONFIG] BusName         = [%s]", cfg.BusName)
+	log.Printf("[CONFIG] BusEventSource  = [%s]", cfg.BusEventSource)
+
+	// database definitions
+	log.Printf("[CONFIG] DbHost          = [%s]\n", cfg.DbHost)
+	log.Printf("[CONFIG] DbPort          = [%d]\n", cfg.DbPort)
+	log.Printf("[CONFIG] DbName          = [%s]\n", cfg.DbName)
+	log.Printf("[CONFIG] DbUser          = [%s]\n", cfg.DbUser)
+	log.Printf("[CONFIG] DbPassword      = [REDACTED]\n")
+
+	return &cfg
+}
---
+++ aptrust-submit-reconciler/worker.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"time"
+
+	"github.com/uvalib/aptrust-submit-bus-definitions/uvaaptsbus"
+	"github.com/uvalib/aptrust-submit-db-dao/uvaaptsdao"
+)
+
+func worker(done chan<- bool, cfg *ServiceConfig, busEvent *uvaaptsbus.UvaBusEvent) {
+
+	start := time.Now()
+	log.Printf("INFO: worker starting")
+
+	// ensure this is the type of event we want to process
+	switch busEvent.EventName {
+	case uvaaptsbus.EventSubmissionReconcile:
+	default:
+		log.Printf("ERROR: unexpected event type (%s), ignoring", busEvent.EventName)
+		done <- true
+		return
+	}
+
+	// make the workflow event
+	wf, err := uvaaptsbus.MakeWorkflowEvent(busEvent.Detail)
+	if err != nil {
+		log.Printf("ERROR: unmarshaling workflow event (%s)", err.Error())
+		done <- false
+		return
+	}
+
+	log.Printf("INFO: EVENT %s / %s", busEvent.String(), wf.String())
+
+	// create our event bus client
+	eventBus, _ := NewEventBus(cfg.BusName, cfg.BusEventSource)
+
+	// create the data access object
+	dao, err := uvaaptsdao.NewDao(cfg.DbHost, cfg.DbPort, cfg.DbUser, cfg.DbPassword, cfg.DbName)
+	if err != nil {
+		log.Printf("ERROR: connecting to the database (%s)", err.Error())
+		done <- false
+		return
+	}
+
+	// cleanup on exit
+	defer dao.Close()
+
+	// get all the files that conflict for this submission
+	files, err := dao.GetConflictFilesBySubmission(wf.SubmissionId)
+	if err != nil {
+		if errors.Is(err, uvaaptsdao.ErrFileNotFound) == false {
+			log.Printf("ERROR: getting submission conflict file set (%s)", err.Error())
+			done <- false
+			return
+		}
+	}
+
+	// for every conflict file in the submission, determine if we can ignore it by checking on the
+	// whitelist
+	reconcileFailures := 0
+	if files != nil {
+
+		// get our whitelisted file set
+		whitelist, err := dao.GetWhitelistedFiles()
+		if err != nil {
+			if errors.Is(err, uvaaptsdao.ErrFileNotFound) == false {
+				log.Printf("ERROR: getting whitelist fileset (%s)", err.Error())
+				done <- false
+				return
+			}
+		}
+
+		// if we have whitelisted files, we may be able to remove files from the conflict set
+		if whitelist != nil {
+			for _, f := range files {
+				w := inWhitelist(whitelist, f.Hash)
+				if w == nil {
+					reconcileFailures++
+				} else {
+					log.Printf("INFO: hash found in whitelist fileset, ignoring [%s]", f.Name)
+				}
+			}
+		} else {
+			reconcileFailures = len(files)
+		}
+	}
+
+	// we are done, publish the appropriate event and terminate
+	if reconcileFailures > 0 {
+		log.Printf("WARNING: %d conflicting files found for submission\n", reconcileFailures)
+		_ = publishWorkflowEvent(eventBus, uvaaptsbus.EventSubmissionReconcileFail, busEvent.ClientId, wf.SubmissionId, wf.BagId)
+	} else {
+		log.Printf("INFO: no conflicting files found for submission\n")
+		_ = publishWorkflowEvent(eventBus, uvaaptsbus.EventSubmissionApprove, busEvent.ClientId, wf.SubmissionId, wf.BagId)
+	}
+
+	duration := time.Since(start)
+	log.Printf("INFO: worker terminating (elapsed %0.2f seconds)\n", duration.Seconds())
+	done <- true
+}
+
+func inWhitelist(whitelist []uvaaptsdao.WhitelistedFile, hash string) *uvaaptsdao.WhitelistedFile {
+	for _, w := range whitelist {
+		if w.Hash == hash {
+			return &w
+		}
+	}
+	return nil
+}
+
+//
+// end of file
+//
