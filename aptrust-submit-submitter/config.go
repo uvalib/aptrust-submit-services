0529f9a4282c7c614069a8934aec6371ef8f74b0
Dave G <dpg3k@virginia.edu>
2026-03-09 14:24:08 -0400

    Initial version of the submitter

---
+++ aptrust-submit-submitter/config.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"log"
+)
+
+// ServiceConfig defines all the service configuration parameters
+type ServiceConfig struct {
+	// queue definitions
+	InQueueName   string // SQS queue name to monitor for message
+	PollTimeOut   int32  // the SQS queue timeout (in seconds)
+	HeartbeatTime int32  // the SQS queue heartbeat time (in seconds)
+
+	// ingest details
+	LocalAssetCache string // the root of the asset cache
+
+	// outbound details
+	APTBucket    string // the outbound bucket name
+	APTAccessKey string // APT access credentials
+	APTSecretKey string // APT access credentials
+
+	// event bus definitions
+	BusName        string // the event bus name
+	BusEventSource string // the source of published events
+
+	// database configuration
+	DbHost     string // database host
+	DbPort     int    // database port
+	DbName     string // database name
+	DbUser     string // database user
+	DbPassword string // database password
+}
+
+// LoadConfiguration will load the service configuration from env/cmdline
+// and return a pointer to it. Any failures are fatal.
+func LoadConfiguration() *ServiceConfig {
+
+	var cfg ServiceConfig
+
+	// queue definitions
+	cfg.InQueueName = ensureSetAndNonEmpty("NOTIFY_IN_QUEUE")
+	cfg.PollTimeOut = int32(envToInt("NOTIFY_QUEUE_POLL_TIMEOUT"))
+	cfg.HeartbeatTime = int32(envToInt("NOTIFY_QUEUE_HEARTBEAT_TIME"))
+
+	// ingest details
+	cfg.LocalAssetCache = ensureSetAndNonEmpty("LOCAL_ASSET_CACHE")
+
+	// outbound details
+	cfg.APTBucket = ensureSetAndNonEmpty("APT_DEPOSIT_BUCKET")
+	cfg.APTAccessKey = ensureSetAndNonEmpty("APT_ACCESS_KEY_ID")
+	cfg.APTSecretKey = ensureSetAndNonEmpty("APT_SECRET_ACCESS_KEY")
+
+	// event bus definitions
+	cfg.BusName = envWithDefault("EVENT_BUS_NAME", "")
+	cfg.BusEventSource = envWithDefault("EVENT_SRC_NAME", "")
+
+	// database definitions
+	cfg.DbHost = ensureSetAndNonEmpty("DB_HOST")
+	cfg.DbPort = envToInt("DB_PORT")
+	cfg.DbName = ensureSetAndNonEmpty("DB_NAME")
+	cfg.DbUser = ensureSetAndNonEmpty("DB_USER")
+	cfg.DbPassword = ensureSetAndNonEmpty("DB_PASSWORD")
+
+	// queue definitions
+	log.Printf("[CONFIG] InQueueName     = [%s]", cfg.InQueueName)
+	log.Printf("[CONFIG] PollTimeOut     = [%d]", cfg.PollTimeOut)
+	log.Printf("[CONFIG] HeartbeatTime   = [%d]", cfg.HeartbeatTime)
+
+	// ingest details
+	log.Printf("[CONFIG] LocalAssetCache = [%s]", cfg.LocalAssetCache)
+
+	// outbound details
+	log.Printf("[CONFIG] APTBucket       = [%s]", cfg.APTBucket)
+	log.Printf("[CONFIG] APTAccessKey    = [REDACTED]")
+	log.Printf("[CONFIG] APTSecretKey    = [REDACTED]")
+
+	// event bus definitions
+	log.Printf("[CONFIG] BusName         = [%s]", cfg.BusName)
+	log.Printf("[CONFIG] BusEventSource  = [%s]", cfg.BusEventSource)
+
+	// database definitions
+	log.Printf("[CONFIG] DbHost          = [%s]", cfg.DbHost)
+	log.Printf("[CONFIG] DbPort          = [%d]", cfg.DbPort)
+	log.Printf("[CONFIG] DbName          = [%s]", cfg.DbName)
+	log.Printf("[CONFIG] DbUser          = [%s]", cfg.DbUser)
+	log.Printf("[CONFIG] DbPassword      = [REDACTED]")
+
+	return &cfg
+}
---
+++ aptrust-submit-submitter/worker.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/uvalib/aptrust-submit-bus-definitions/uvaaptsbus"
+	//"github.com/uvalib/aptrust-submit-db-dao/uvaaptsdao"
+)
+
+func worker(done chan<- bool, cfg *ServiceConfig, busEvent *uvaaptsbus.UvaBusEvent) {
+
+	start := time.Now()
+	log.Printf("INFO: worker starting")
+
+	// ensure this is the type of event we want to process
+	switch busEvent.EventName {
+	case uvaaptsbus.EventBagSubmit:
+	default:
+		log.Printf("ERROR: unexpected event type (%s), ignoring", busEvent.EventName)
+		done <- true
+		return
+	}
+
+	// make the workflow event
+	wf, err := uvaaptsbus.MakeWorkflowEvent(busEvent.Detail)
+	if err != nil {
+		log.Printf("ERROR: unmarshaling workflow event (%s)", err.Error())
+		done <- false
+		return
+	}
+
+	log.Printf("INFO: EVENT %s / %s", busEvent.String(), wf.String())
+
+	// create our event bus client
+	eventBus, _ := NewEventBus(cfg.BusName, cfg.BusEventSource)
+
+	// create the data access object
+	//dao, err := uvaaptsdao.NewDao(cfg.DbHost, cfg.DbPort, cfg.DbUser, cfg.DbPassword, cfg.DbName)
+	//if err != nil {
+	//	log.Printf("ERROR: connecting to the database (%s)", err.Error())
+	//	done <- false
+	//	return
+	//}
+
+	// cleanup on exit
+	//defer dao.Close()
+
+	// get the necessary bag
+	//bag, err := dao.GetBagBySubmissionAndName(wf.SubmissionId, wf.BagId)
+	//if err != nil {
+	//	log.Printf("ERROR: getting bag information (%s)", err.Error())
+	//	done <- false
+	//	return
+	//}
+
+	// assets in <cache root>/<clientId>/<submissionId>/...
+	bagFile := fmt.Sprintf("%s.tar", wf.BagId)
+	assetName := fmt.Sprintf("%s/%s/%s/%s", cfg.LocalAssetCache, busEvent.ClientId, wf.SubmissionId, bagFile)
+
+	// ensure it exists
+	if _, err := os.Stat(assetName); errors.Is(err, os.ErrNotExist) {
+		log.Printf("ERROR: bag file [%s] does not exist (%s)", assetName, err.Error())
+		done <- false
+		return
+	}
+
+	// create custom credentials
+	creds := credentials.NewStaticCredentialsProvider(cfg.APTAccessKey, cfg.APTSecretKey, "")
+
+	// create our s3 helper client
+	s3Client, err := newS3Client(&creds)
+	if err != nil {
+		log.Printf("ERROR: creating s3 client (%s)", err.Error())
+		done <- false
+		return
+	}
+
+	// upload to the APTrust deposit bucket
+	err = s3Client.s3Put(cfg.APTBucket, bagFile, assetName)
+	if err != nil {
+		log.Printf("ERROR: uploading bag file [%s] (%s)", assetName, err.Error())
+		done <- false
+		return
+	}
+
+	// get the object ETAG so we can refer to it later...
+	res, err := s3Client.s3Head(cfg.APTBucket, bagFile)
+	if err != nil {
+		log.Printf("ERROR: getting attributes for [%s] (%s)", bagFile, err.Error())
+		done <- false
+		return
+	}
+
+	log.Printf("INFO: ETag for [%s] => (%s)", bagFile, *res.ETag)
+
+	// we are done, publish the appropriate event and terminate
+	_ = publishWorkflowEvent(eventBus, uvaaptsbus.EventBagSubmitted, busEvent.ClientId, wf.SubmissionId, wf.BagId)
+
+	duration := time.Since(start)
+	log.Printf("INFO: worker terminating (elapsed %0.2f seconds)", duration.Seconds())
+	done <- true
+}
+
+//
+// end of file
+//
